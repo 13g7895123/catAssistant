561366874df9d94b46b23e2b124d300448caac74
Jarvis <13g7895123@gmail.com>
2023-11-20 17:04:05 +0800

    [edit] revise to new class

--- actions/actions.php
+++ actions/actions.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once(__DIR__ . '/reincarnationRecords.php');
+include_once(__DIR__ . '/frenzyTotem.php');
 
 class default_action
 {
@@ -21,6 +21,19 @@ class default_action
                     reincarnationRecords::finish($rR_data);
                     break;
             }
+        }else{
+            /* 取得目前動作 */
+            MYPDO::$table = 'action';
+            MYPDO::$order = ['time' => 'desc'];
+            $result = MYPDO::first();
+
+            /* 驗證動作時間是否超時 */
+
+            /* 執行動作 */
+            $data = [];
+            $data['message'] = $message;
+            $data['token'] = $token;
+            frenzyTotem::finish($data);
         }
     }
 }
